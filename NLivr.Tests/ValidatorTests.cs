749c81f3d9d23812f724c6e065e0971b4707df9e
Alex Anisimoff <alex.anisimoff@gmail.com>
2019-04-14 00:47:05 +0300

    added create validator tests

--- NLivr.Tests/ValidatorTests.cs
+++ NLivr.Tests/ValidatorTests.cs
@@ -6,9 +6,21 @@ namespace NLivr.Tests
     public class ValidatorTests
     {
         [Fact]
-        public void Test1()
+        public void Validator_Create_ThrowExceptionIfRulesEmpty()
         {
+           Assert.Throws<ArgumentNullException>(()=>Validator.Create(null));
+        }
 
+        [Fact]
+        public void Validator_Create_ThrowExceptionIfRulesNotJsonString()
+        {
+            Assert.Throws<ArgumentException>(() => Validator.Create("test"));
+        }
+
+        [Fact]
+        public void Validator_Create_ReturnValidator()
+        {
+           Assert.NotNull( Validator.Create("{}"));
         }
     }
 }
--- NLivr/Validator.cs
+++ NLivr/Validator.cs
@@ -1,6 +1,29 @@
-﻿namespace NLivr
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NLivr
 {
     public class Validator
     {
+        private Validator()
+        {
+            
+        }
+
+        public static Validator Create(string rules)
+        {
+            if (rules == null) throw new ArgumentNullException(nameof(rules));
+
+            try
+            {
+                var  jobject = JObject.Parse(rules);
+            }
+            catch (JsonReaderException exception)
+            {
+                throw new ArgumentException(nameof(rules), exception);
+            }
+            return  new Validator();
+        }
     }
 }
